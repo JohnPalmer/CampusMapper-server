78e9f3cf17b1159266fe4a1eb3cfbd36db2a04b6
John Palmer <johnrbpalmer@gmail.com>
2015-03-05 20:42:50 +0100

    more formatting for user codes

--- campus_mapper_server/urls.py
+++ campus_mapper_server/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     url(r'^api/', include('tracks.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^mobility_mapper_code/', show_user_code, name='show_user_code'),
+    url(r'^$', show_user_code),
 )
 
