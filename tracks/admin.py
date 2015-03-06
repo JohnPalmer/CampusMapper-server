3cc43a3b4d408503b1e8ba1f9e3ab81e87ef6db7
John Palmer <johnrbpalmer@gmail.com>
2015-03-06 12:56:42 +0100

    modified adin site so data cannot be changed from there

--- tracks/admin.py
+++ tracks/admin.py
@@ -4,6 +4,14 @@ from tracks.models import *
 
 class DataPointAdmin(admin.ModelAdmin):
     list_display = ('user_UUID', 'user_code', 'type', 'last_modified', 'created',)
+    readonly_fields = ('user_UUID', 'user_code', 'type', 'encrypted_data', 'last_modified', 'created',)
+    fields = ('user_UUID', 'user_code', 'type', 'encrypted_data', 'last_modified', 'created',)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(DataPoint, DataPointAdmin)
