60dbd3b7e1e07b904a9705173d1924cc9b8cd208
John Palmer <johnrbpalmer@gmail.com>
2015-03-06 12:58:12 +0100

    bug fix

--- tracks/admin.py
+++ tracks/admin.py
@@ -4,8 +4,8 @@ from tracks.models import *
 
 class DataPointAdmin(admin.ModelAdmin):
     list_display = ('user_UUID', 'user_code', 'type', 'last_modified', 'created',)
-    readonly_fields = ('user_UUID', 'user_code', 'type', 'encrypted_data', 'last_modified', 'created',)
-    fields = ('user_UUID', 'user_code', 'type', 'encrypted_data', 'last_modified', 'created',)
+    readonly_fields = ('user_UUID', 'user_code', 'type', 'encrypted_message', 'last_modified', 'created',)
+    fields = ('user_UUID', 'user_code', 'type', 'encrypted_message', 'last_modified', 'created',)
 
     def has_add_permission(self, request):
         return False
