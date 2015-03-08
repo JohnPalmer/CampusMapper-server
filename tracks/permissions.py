63cc6417dc3a5992da0190e537d1cb21c6b464e8
John Palmer <johnrbpalmer@gmail.com>
2015-03-08 14:42:15 +0100

    bug fix

--- tracks/permissions.py
+++ tracks/permissions.py
@@ -9,6 +9,6 @@ class TracksPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         ip_addr = request.META['REMOTE_ADDR']
-        allowed_ip = AllowedIPs.objects.filter(ip_addr=ip_addr).exists()
+        allowed_ip = AllowedIPs.objects.filter(IP_address=ip_addr).exists()
         allowed_group = 'data_access_group' in [group.name for group in request.user.groups.all()]
         return allowed_ip and allowed_group
