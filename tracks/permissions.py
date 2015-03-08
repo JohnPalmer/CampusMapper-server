1b97a751bc2e69ea96a51efd10060cba241af96f
John Palmer <johnrbpalmer@gmail.com>
2015-03-08 14:55:57 +0100

    bug fix

--- tracks/permissions.py
+++ tracks/permissions.py
@@ -8,7 +8,11 @@ class TracksPermission(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        ip_addr = request.META['REMOTE_ADDR']
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip_addr = x_forwarded_for.split(',')[-1].strip()
+        else:
+            ip_addr = request.META.get('REMOTE_ADDR')
         allowed_ip = AllowedIPs.objects.filter(IP_address=ip_addr).exists()
         allowed_group = 'data_access_group' in [group.name for group in request.user.groups.all()]
         return allowed_ip and allowed_group
